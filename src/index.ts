4634d9c0ff43195c29fa5e8f57a13740f8d322d6
Jan Drewniak <jan.drewniak@gmail.com>
2020-10-11 15:36:06 +0200

    Adding basic support for Osram GU10 white bulbs.

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,7 @@ import { IkeaMotionSensor } from './accessories/ikea/ikea-motion-sensor';
 import { LinkindMotionSensor } from './accessories/linkind/linkind-motion-sensor';
 import { NamronDimmer } from './accessories/namron/namron-dimmer';
 import { NamronSwitch } from './accessories/namron/namron-switch';
+import { OsramSmartPlusBulbWhite } from './accessories/osram/osram-smart-plus-bulb';
 
 function registerSupportedDevices() {
   registerAccessoryClass('GLEDOPTO', ['GL-C-009'], GledoptoDim);
@@ -113,16 +114,33 @@ function registerSupportedDevices() {
   );
 
   registerAccessoryClass('Xiaomi', ['WSDCGQ01LM', 'WSDCGQ11LM'], TempHumiSensor);
-  registerAccessoryClass('Xiaomi', ['lumi.sensor_magnet', 'lumi.sensor_magnet.aq2'], XiaomiContactSensor);
+  registerAccessoryClass(
+    'Xiaomi',
+    ['lumi.sensor_magnet', 'lumi.sensor_magnet.aq2'],
+    XiaomiContactSensor
+  );
   registerAccessoryClass('Xiaomi', ['GZCGQ01LM'], XiaomiLightIntensitySensor);
-  registerAccessoryClass('LUMI', ['lumi.weather', 'lumi.sensor_ht.agl02', 'lumi.sensor_ht'], TempHumiSensor);
-  registerAccessoryClass('LUMI', ['lumi.sensor_magnet', 'lumi.sensor_magnet.aq2'], XiaomiContactSensor);
+  registerAccessoryClass(
+    'LUMI',
+    ['lumi.weather', 'lumi.sensor_ht.agl02', 'lumi.sensor_ht'],
+    TempHumiSensor
+  );
+  registerAccessoryClass(
+    'LUMI',
+    ['lumi.sensor_magnet', 'lumi.sensor_magnet.aq2'],
+    XiaomiContactSensor
+  );
   registerAccessoryClass('LUMI', ['lumi.sen_ill.mgl01'], XiaomiLightIntensitySensor);
-  registerAccessoryClass('LUMI', ['lumi.sensor_switch.aq2', 'lumi.sensor_switch'], XiaomiWirelessSwitch);
+  registerAccessoryClass(
+    'LUMI',
+    ['lumi.sensor_switch.aq2', 'lumi.sensor_switch'],
+    XiaomiWirelessSwitch
+  );
   registerAccessoryClass('TuYa', ['GDKES-02TZXD'], TuyaOnoffDoubleSwitch);
   registerAccessoryClass('lk', ['ZB-MotionSensor-D0003'], LinkindMotionSensor);
   registerAccessoryClass('NAMRON AS', ['4512700', '1402755'], NamronDimmer);
   registerAccessoryClass('NAMRON AS', ['4512704'], NamronSwitch);
+  registerAccessoryClass('OSRAM', ['AA68199', '4058075148338'], OsramSmartPlusBulbWhite);
 }
 
 /**
